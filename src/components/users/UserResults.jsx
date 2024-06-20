15a14ab1147e009f2ec7cdd573f415fcbddc94a3
Anara <berdali.aygerim9@gmail.com>
2024-06-20 19:07:35 +0100

    UserResults updated with GithubContext

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,32 +1,15 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GitHubContext";
 
 function UserResults() {
-  //empty array that will be filled with users 
-  const [users, setUsers] = useState([])
-  //whenever request is sent to api we need loader
-  //while api is loading its true, when we get data NOT loading any more = false
-  const [loading, setLoading] = useState(true)
+  const {users, loading, fetchUsers} = useContext(GithubContext)
 
   useEffect(() => {
     fetchUsers()
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(
-      `${import.meta.env.VITE_APP_GITHUB_URL}/users`,
-      {
-        headers: {
-          Authorization: `token ${import.meta.env.VITE_APP_GITHUB_TOKEN}`
-        },
-      });
-
-      const data = await response.json()
-      setUsers(data)
-      setLoading(false)
-  };
 
   if(!loading){
     return <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
--- src/context/github/GitHubContext.jsx
+++ src/context/github/GitHubContext.jsx
@@ -29,6 +29,7 @@ export const GithubProvider = ({ children }) => {
       value={{
         users,
         loading,
+        fetchUsers,
       }}
     >
       {children}
