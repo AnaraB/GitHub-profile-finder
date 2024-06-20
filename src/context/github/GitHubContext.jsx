d538c5c1816ed6218e24436c06216250cbf57180
Anara <berdali.aygerim9@gmail.com>
2024-06-20 19:00:04 +0100

    github context set up

---
+++ src/context/github/GitHubContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = import.meta.env.VITE_APP_GITHUB_URL;
+const GITHUB_TOKEN = import.meta.env.VITE_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  //empty array that will be filled with users
+  const [users, setUsers] = useState([]);
+  //whenever request is sent to api we need loader
+  //while api is loading its true, when we get data NOT loading any more = false
+  const [loading, setLoading] = useState(true);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+    setUsers(data);
+    setLoading(false);
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users,
+        loading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext 
